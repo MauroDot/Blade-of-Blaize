047f1f125b985e87e175d87761293897b82ad887
MauroDot <63043182+MauroDot@users.noreply.github.com>
2023-10-31 13:39:21 -0500

    started IDamageable process

--- Assets/Scripts/Enemy/Skeleton.cs
+++ Assets/Scripts/Enemy/Skeleton.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : Enemy
+public class Skeleton : Enemy, IDamageable
 {
+    public int Health { get; set; }
     public override void Init()
     {
         base.Init();
+        Health = base.health;
+    }
+
+    public void Damage()
+    {
+
     }
 }
--- Assets/Scripts/Enemy/Spider.cs
+++ Assets/Scripts/Enemy/Spider.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spider : Enemy
+public class Spider : Enemy, IDamageable
 {
+    public int Health { get; set; }
     public override void Init()
     {
         base.Init();
+        Health = base.health;
+    }
+
+    public void Damage()
+    {
+
     }
 }
